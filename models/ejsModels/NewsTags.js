2180a5ba98c413c6433d6cc1937b00d744a377ec
= <=>
2019-05-12 16:02:36 +0700

    Fix search by tags

--- models/ejsModels/NewsTags.js
+++ models/ejsModels/NewsTags.js
@@ -4,10 +4,10 @@ var result = (tags) => {
     var res = '<p class="tag-list">'
     var tagList = StringtoTagList(tags)
     for(var i = 0; i<tagList.length; i++){
-		res += '<a href=' + "/search?key="+tagList[i]+ '>' + tagList[i] +'</a>'
+		res += '<a href=' + "/search?key="+tagList[i].slice(1)+ '>' + tagList[i] +'</a>'
     }
-    res+='</p>';
-    return res;
+    res+='</p>'
+    return res
 }
 
 module.exports = result;
